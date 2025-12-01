d03a50a79655715449846dd2ef55b287bd074b72
Chris Thayil <cjt66@njit.edu>
2025-12-01 10:33:37 -0500

    Create auth.service.ts
    Update Frontend (Angular) to Connect to Backend

---
+++ src/app/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://192.168.1.101:3000'; // Use your backend VM IP
+
+  constructor(private http: HttpClient) {}
+
+  register(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, { email, password });
+  }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+}
