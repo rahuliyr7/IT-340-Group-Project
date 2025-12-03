461449bc15db6606a88fb3ddd505fa8dc0582655
rahuliyr7 <rti@njit.edu>
2025-12-03 10:21:03 -0500

    Add two-factor authentication fields to User model
    Added fields for two-factor authentication to user schema.

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
+
+  //2FA Fields added by Rahul
+  twoFactorCode: {type:String}, //6 digit code
+  twoFactorExpiry: {type:Date} //code expiry
 });
 
 module.exports = mongoose.model('User', userSchema);
