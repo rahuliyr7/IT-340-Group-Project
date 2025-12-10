c9d8f9bdd4983a5e29b6f780a7aa1f8715db9519
miraz05 <psm6@njit.edu>
2025-12-09 23:29:37 -0500

    removed some lines

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
