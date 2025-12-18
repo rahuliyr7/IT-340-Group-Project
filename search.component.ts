ddbfd6fc2b112d7c733c77118b0612a35e0fbfdb
Alejandro Rea <ar2848@njit.edu>
2025-12-18 13:14:47 -0500

    added loadCategories and setCategories method and having support for them in the search method to search.component.ts

--- search.component.ts
+++ search.component.ts
@@ -1,4 +1,6 @@
 // search.component.ts
+categories: string[] = [];
+selectedCategory = '';
 availableTags = ['collectibles', 'rare', 'vintage', 'electronics']; // Or fetch from API
 selectedTags: string[] = [];
 searchTerm = '';
@@ -7,6 +9,8 @@ search() {
   let params = new HttpParams();
   if (this.searchTerm) params = params.set('search', this.searchTerm);
   if (this.selectedTags.length) params = params.set('tags', this.selectedTags.join(','));
+  if (this.selectedCategory) params = params.set('category', this.selectedCategory);
+
 
   this.http.get('/api/products', { params }).subscribe((products: any) => {
     this.products = products;
@@ -20,3 +24,20 @@ updateTags(event: any) {
     this.selectedTags = this.selectedTags.filter(t => t !== event.target.value);
   }
 }
+loadCategories() {
+  this.productService.getCategories().subscribe({
+    next: (res: any) => {
+      this.categories = res.categories;
+    },
+    error: err => console.error('Failed to load categories', err)
+  });
+}
+ngOnInit() {
+  this.loadCategories();
+}
+setCategory(category: string) {
+  this.selectedCategory = category;
+  this.search();
+}
+
+
